9a33d0421198f33efe53be62924bf9c3090a3e87
ChenYu-Chieh <cloud550exc@gmail.com>
2020-02-09 00:31:01 +0800

    新增表單格 with RWD

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 window.onload = function(){
 
 	//alert('Hello World!')
 	
-=======
-window.onload = function(){
-
-	alert('Hello World!')
->>>>>>> 3b30767d9cde4f824aafac02babb2352f2c126db
 }
