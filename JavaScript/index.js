a4eb73f3d2ea22be8becd5c971b24cafd301b9ee
ChenYu-Chieh <cloud550exc@gmail.com>
2020-03-05 18:10:22 +0800

    Day 28 提升使用者體驗ing

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,9 +5,16 @@ window.onload = function(){
 		$("#facebook").tooltip({ title:"go to FB",placement:"right"});
 		$("#instagram").tooltip({title:"go to instagram",placement:"right"});
 		$("#Github").tooltip({title:"go to Github",placement:"right"});
-		
+	
+//作品引導按鈕
+	$("#guidebtn:nth-child(1)").click(function(){
 
+	});
+	$("#guidebtn:nth-child(2)").click(function(){
+		$("#Carousel").show();
+	});
 
+	//作品輪播區
 		// Activate Carousel
 		$('#myCarousel').carousel();
 
@@ -30,25 +37,25 @@ window.onload = function(){
 			$("#myCarousel").carousel("next");
 		});
 
-		//浮動選單 取消按鈕事件
+	//浮動選單 取消按鈕事件
 		$("#x1").click(function(){
 	//加callback就不會有自動回復問題
 			$("#menu-float").hide("slow",function(){
 				$(this).remove();
 			});
 		});
-		//用for迴圈能重複效果使用吧?
+	//用for迴圈能重複效果使用吧?
 		$(window).scroll(function(){
 			var scrollVal = $(this).scrollTop();
-			if (scrollVal > 30){ 
-			$("#menu-float").animate({top:"150px"},"slow");
+			if (scrollVal > 200){ 
+			$("#menu-float").animate({top:"150px"},500);
 			//$("#menu-float").animate({top:"0px"},"slow");
 			// if ($(window).scrollTop(0,10)){
 			// 	$("#menu-float").animate({top:"-50px"},"slow")
 			// 	.animate({top:"0px"},"slow");
 			// 	} else ($(documnet).scrollBy(0,-10)){
-			} else if (scrollVal < 30){
-				$("#menu-float").animate({top:"50px"},"slow");
+			} else if (scrollVal < 200){
+				$("#menu-float").animate({top:"50px"},500);
 			}
 		});
 
